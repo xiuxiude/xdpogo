c483ac1bf51bbe151d5fdd1c25450382cfe08952
Sean Lee <iseansay@gmail.com>
2013-03-04 22:23:10 +0800

    added events route, and only use :show action in :users and :events resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Xdpogo::Application.routes.draw do
   root to: 'welcome#index'
-  resources :users
+  resources :users, :only => [:show]
+  resources :events, :only => [:show]
   # get '/auth/:provider/callback', to: 'sessions#create', as: 'callback'
   # get '/auth/failure', to: 'sessions#error', as: 'failure'
   # delete '/signout', to: 'sessions#destroy', as: 'signout'
