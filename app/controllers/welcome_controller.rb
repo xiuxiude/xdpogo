5acb35a9d832e03cc635d4e96cd2ab69900c110c
Sean Lee <iseansay@gmail.com>
2013-03-06 21:20:59 +0800

    use eager-load to improve proformance and add a :future scope to event model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,6 @@ class WelcomeController < ApplicationController
   caches_action :index
 
   def index
-    @events = Event.where("begin_time>=:date", date:Time.now)
-                    .order("begin_time ASC")
+    @events = Event.future.order("begin_time ASC").includes(:wishers, :participanters)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,8 @@ class Event < ActiveRecord::Base
                   :geo, :address, :album, :participant_count, :wisher_count,
                   :begin_time, :end_time, :content, :owner_id
 
+  scope :future, -> { where("begin_time >= ?", Time.now) }
+
   def self.find_with_douban(event_info)
     where(event_info.slice(:id)).first_or_initialize.tap do |event|
       event.id = event_info.id
