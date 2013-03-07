fb3e269db69901052ad3bd822c3503770de2ae11
Sean Lee <iseansay@gmail.com>
2013-03-07 20:46:58 +0800

    inplement event page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,8 +8,7 @@ Xdpogo::Application.configure do
   
   # eager loading
   config.eager_load = false
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
+
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Xdpogo::Application.config.secret_token = ENV['SESSION_SECRET']
+Xdpogo::Application.config.secret_token = ENV['SESSION_SECRET'] || "68ad6e10103c2f6cd8c1560774baf9aceda9a2fc389b9342283d7ebb1fa37dd749f7a5cfe0e6bcf6965b576cae081871ff4a9580dec282d17fe4613301b3033d"
+
